b768a72a5a6e797b8adfb9f8499bd38eef83b751
Eds <eds@edss-mbp-2.home>
2021-01-31 14:19:34 +0000

    Function to get currencies added

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,3 +6,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from time import sleep # insert pauses in code
 import pandas as pd
+
+# Writing a function to scrape the data. It will receive
+# List of currency codes
+# Start & end date
+# Boolean for exporting data as .csv
+
+def get_currencies(currencies, start, end, export_csv=False):
+    frames = []
+    for currency in currencies:
+    my_url = f'https://br.investing.com/currencies/usd-{currency.lower()}-historical-data'
+    option = Options()
+    option.headless = False
+    driver = webdriver.Chrome(options=option)
+    driver.get(my_url)
+    driver.maximize_window()
+
+
+
+
+# Tutorial from https://www.freecodecamp.org/news/how-to-code-a-scraping-bot-with-selenium-and-python/
