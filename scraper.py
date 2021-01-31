d8612116839bf244957b64bcb68f322a8df159a3
Eds <eds@edss-mbp-2.home>
2021-01-31 14:04:46 +0000

    Scraper added, with imports

---
+++ scraper.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep # insert pauses in code
+import pandas as pd
